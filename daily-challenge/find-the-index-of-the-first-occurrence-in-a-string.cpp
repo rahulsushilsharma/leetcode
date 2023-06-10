1426994dae6058d5f71c69e76305ff529c3afada
Rahul Sharma <rahu8299@gmail.com>
2023-06-11 00:09:19 +0530

    setting vscode

--- daily-challenge/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ daily-challenge/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int i = 0;
-        int j = 0;
-        int flag = 1;
-        int cur = 0;
+  int strStr(string haystack, string needle) {
+    int i = 0;
+    int j = 0;
+    int flag = 1;
+    int cur = 0;
 
-        while(i<haystack.size()){
-            cur = 0;
-            
-            if(haystack[i] == needle[0]){
-                cur = i;
-                flag = 0;
-                while(j<needle.size() && i<haystack.size()){
-                    cout<<haystack[i]<<' '<<needle[j]<<endl;
-                    if(haystack[i++] != needle[j++]){flag = 0;cout<<"break"<<endl;break;}
-                }
-                if(flag==0 || i>=haystack.size())return cur;
-                j = 0;
-            }
+    while (i < haystack.size()) {
+      cur = 0;
+
+      if (haystack[i] == needle[0]) {
+        cur = i;
+        flag = 0;
+        while (j < needle.size() && i < haystack.size()) {
+          cout << haystack[i] << ' ' << needle[j] << endl;
+          if (haystack[i++] != needle[j++]) {
+            flag = 0;
+            cout << "break" << endl;
+            break;
+          }
         }
-        return -1;
+        if (flag == 0 || i >= haystack.size())
+          return cur;
+        j = 0;
+      }
     }
+    return -1;
+  }
 };
