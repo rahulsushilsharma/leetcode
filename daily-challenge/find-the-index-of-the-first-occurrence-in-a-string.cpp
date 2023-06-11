304a7c7d35e2b69316ac08d1eaedabe04cab886a
Rahul Sharma <rahu8299@gmail.com>
2023-06-11 09:02:37 +0530

    optamized

--- daily-challenge/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ daily-challenge/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,13 +4,17 @@ public:
     int i = 0;
     int j = 0;
     int k = 0;
-    while(i<haystack.size() && j<needle.size()){
+    int flag = 0;
+    int cur = 0;
+    int n = haystack.size();
+    int m = needle.size();
+    while(i<n && j<m){
         if(haystack[i] == needle[0]){
-            int flag = 0;
-            int cur = i;
+            flag = 0;
+            cur = i;
             j = i;
-            while(k<needle.size()){
-                if(j>=haystack.size())return -1;
+            while(k<m){
+                if(j>=n)return -1;
                 if(haystack[j]!=needle[k]){
                     j=0;
                     k = 0;
