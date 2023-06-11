db3f2f6f48829f284cf911f68e136389711ee2f8
Rahul Sharma <rahu8299@gmail.com>
2023-06-11 22:07:32 +0530

    pending

--- daily-challenge/maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ daily-challenge/maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-    int maxValue(int n, int index, int maxSum) {
-        
+  int maxValue(int n, int index, int maxSum) {
+
+    int mul = 1;
+
+    int size = n;
+    if (index >= 1 && index <= n - 2)
+      mul = 2;
+    int x = 0;
+    int y = 0;
+    while (size >= 1) {
+
+      x = size + ((size - 1) * mul);
+
+      y = 3 - mul;
+      cout << x << " " << y << " " << size << " " << x + y << " " << maxSum
+           << endl;
+      if (x + y <= maxSum)
+        return size;
+
+      size--;
     }
+    return -1;
+  }
 };
